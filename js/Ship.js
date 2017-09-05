824c98ad7e67ff6936b04c5f243f99205d51d242
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-05 14:16:53 +0200

    ship

---
+++ js/Ship.js
@@ -0,0 +1,76 @@
+function Ship(){
+    this.r = 0.04;
+    this.rear_a = 50;
+    this.a = 0;
+    //
+    this.x = VAR.W/2;
+    this.y = VAR.H/2;
+    //
+    this.modX = 0;
+    this.modY = 0;
+    //
+    this.acc = 0.0004;
+    //
+    this.maxMod = 0.019;
+    //
+    this.points = [{},{},{}];
+}
+
+Ship.prototype.draw = function(){
+    if(Game.key_37 || Game.key_39){
+        this.a = this.a + 7*( Game.key_37 ? -1 : 1 );
+    }
+    if(Game.key_38){
+        this.modX = Math.max(-this.maxMod*VAR.d, Math.min(this.maxMod*VAR.d, this.modX+Math.sin(Math.PI/180*this.a)*this.acc*VAR.d));
+        this.modY = Math.max(-this.maxMod*VAR.d, Math.min(this.maxMod*VAR.d, this.modY-Math.cos(Math.PI/180*this.a)*this.acc*VAR.d));
+    }else{
+        this.modX = this.modX*0.98;
+        
+        this.modX = Math.abs(this.modX)<0.0001 ? 0 : this.modX;
+        
+        this.modY = this.modY*0.98;
+        
+        this.modY = Math.abs(this.modY)<0.0001 ? 0 : this.modY;
+    }
+    
+    this.x+=this.modX;
+    this.y+=this.modY;
+    
+    Game.ctx.beginPath();
+    for(var i = 0; i<3;i++){
+        this.tmp_a = i===0 ? this.a : (this.a+180+(i==1 ? this.rear_a : -this.rear_a));
+        
+        this.tmp_r = i===0 ? this.r : this.r*0.6;
+        
+        this.points[i].x = Math.sin(Math.PI/180*this.tmp_a)*this.tmp_r*VAR.d+this.x;
+        this.points[i].y =
+        -Math.cos(Math.PI/180*this.tmp_a)*this.tmp_r*VAR.d+this.y;
+        //
+        //
+        Game.ctx[ i===0 ? 'moveTo' : 'lineTo' ](this.points[i].x, this.points[i].y);
+    }
+    Game.ctx.closePath();
+    
+    Game.ctx.stroke();
+    
+    
+    Game.ctx.beginPath();
+    if(Game.key_38 && this.draw_thrust){
+        this.draw_thrust = false;
+        for(var i=0; i<3; i++){
+            this.tmp_a = i!=1 ? this.a+180+(i===0 ? -this.rear_a+14 : this.rear_a-14) : this.a+180;
+            this.tmp_r = i==1 ? this.r : this.r*0.5;
+            Game.ctx[i===0 ? 'moveTo' : 'lineTo'](
+                Math.sin(Math.PI/180*this.tmp_a)*this.tmp_r*VAR.d+this.x,
+                -Math.cos(Math.PI/180*this.tmp_a)*this.tmp_r*VAR.d+this.y
+            );
+        }
+        Game.ctx.stroke();
+    }else if(Game.key_38 && !this.draw_thrust){
+             this.draw_thrust = true;
+             }
+    
+    
+}
+    
+//przerwane 13:42
---
+++ js/game.js
@@ -0,0 +1,76 @@
+window.onload = function(){
+    Game.init();
+};
+
+VAR = {
+    fps:60,
+    W:0,
+    H:0,
+    lastTime:0,
+    lastUpdate:-1,
+    rand:function(min,max){
+        return Math.floor(Math.random()*(max-min+1))+min;
+    }
+};
+Game = {
+   init:function(){
+       Game.canvas = document.createElement('canvas');
+       Game.ctx = Game.canvas.getContext('2d');
+       Game.layout();
+       
+       window.addEventListener('resize', Game.layout, false);
+       
+       document.body.appendChild(Game.canvas);
+       
+       Game.ship = new Ship();
+       
+       window.addEventListener('keydown', Game.onKey, false);
+       window.addEventListener('keyup', Game.onKey, false);
+       
+       Game.animationLoop();
+   },
+    onKey:function(event){
+        
+        if(event.keyCode==32 || event.keyCode==37 || event.keyCode==38 || event.keyCode==39){
+            event.preventDefault();
+            
+            if(event.type=='keydown' && !Game['key_'+event.keyCode]){
+                console.log(event.keyCode);
+                Game['key_'+event.keyCode] = true;
+                if(event.keyCode==37){
+                    Game.key_39 = false;
+                }else if(event.keyCode==39){
+                    Game.key_37 = false;
+                }
+            }else if(event.type=='keyup'){
+                Game['key_'+event.keyCode] = false;
+            }
+        }
+    },
+    layout:function(ev){
+        VAR.H = window.innerHeight;
+        VAR.W = window.innerWidth;
+        //
+        VAR.d = Math.min(VAR.W, VAR.H);
+        //
+        Game.canvas.width = VAR.W;
+        Game.canvas.height = VAR.H;
+        //
+        Game.ctx.fillStyle = 'white';
+        Game.ctx.strokeStyle = 'white';
+        Game.ctx.lineWidth = 3;
+        Game.ctx.lineJoin = 'round';
+        
+        
+    },
+    animationLoop:function(time){
+        requestAnimationFrame(Game.animationLoop);
+        if(time-VAR.lastTime>=1000/VAR.fps){
+            VAR.lastTime=time;
+            
+            Game.ctx.clearRect(0,0,VAR.W, VAR.H);
+            
+            Game.ship.draw();
+        }
+    }
+}
